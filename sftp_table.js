f76a257737fc021f017c85646a2c222b3db1daec
Ykrop3000 <grisha.andreev.03@gmail.com>
2023-09-08 16:27:44 +0300

    fix bug

--- sftp_table.js
+++ sftp_table.js
@@ -1,17 +1,10 @@
 const core = require("@actions/core");
 const fs = require("fs");
-const { Deployer } = require("./lib/deployer");
 const { get_document } = require("./lib/google_sheets");
 const { get_client, execute } = require("./lib/ssh");
+const path = require("path");
 
-const options = {
-	dryRun: JSON.parse(core.getInput("dryRun")), // Enable dry-run mode. Default to false.
-	exclude: core.getInput("exclude").split(","), // exclude patterns (glob) like .gitignore.
-	forceUpload: JSON.parse(core.getInput("forceUpload")), // Force uploading all files, Default to false(upload only newer files).
-	removeExtraFilesOnServer: JSON.parse(
-		core.getInput("removeExtraFilesOnServer")
-	), // Remove extra files on server, default to false.
-};
+let Client = require("ssh2-sftp-client");
 
 (async function () {
 	const doc = get_document(
@@ -23,22 +16,90 @@ const options = {
 	const sheet = doc.sheetsByIndex[0];
 	const rows = await sheet.getRows();
 	rows.forEach((row) => {
-		const config = {
-			host: row.get("host"), // Required.
-			port: row.get("port"), // Optional, Default to 22.
-			username: row.get("username"), // Required.
-			password: row.get("password"), // Optional.
-			privateKey: "", // Optional.
-			passphrase: "", // Optional.
-			algorithms: {}, // Optional. Default to false.
-			agent: "", // Optional, path to the ssh-agent socket.
-			localDir: core.getInput("localDir"), // Required, Absolute or relative to cwd.
-			remoteDir: row.get("remoteDir"), // Required, Absolute path only.
-		};
+		let sftp = new Client();
 
-		new Deployer(config, options)
-			.sync()
-			.then(() => console.log("sftp upload success!"));
+		const host = row.get("host");
+		const port = parseInt(row.get("port"));
+		const username = row.get("username");
+		const password = row.get("password");
+		const agent = core.getInput("agent");
+		const privateKeyIsFile = core.getInput("privateKeyIsFile");
+		const passphrase = core.getInput("passphrase");
+
+		var privateKey = core.getInput("privateKey");
+
+		core.setSecret(password);
+		if (passphrase != undefined) {
+			core.setSecret(passphrase);
+		}
+
+		if (privateKeyIsFile == "true") {
+			var privateKey = fs.readFileSync(privateKey);
+			core.setSecret(privateKey);
+		}
+
+		const localPath = core.getInput("localPath");
+		const remotePath = row.get("remotePath");
+		const additionalPaths = core.getInput("additionalPaths");
+
+		sftp
+			.connect({
+				host: host,
+				port: port,
+				username: username,
+				password: password,
+				agent: agent,
+				privateKey: privateKey,
+				passphrase: passphrase,
+			})
+			.then(async () => {
+				console.log("Connection established.");
+				console.log("Current working directory: " + (await sftp.cwd()));
+				await processPath(localPath, remotePath); //TODO: Instead of localPath, remotePath use key/value to uplaod multiple files at once.
+
+				const parsedAdditionalPaths = (() => {
+					try {
+						const parsedAdditionalPaths = JSON.parse(additionalPaths);
+						return Object.entries(parsedAdditionalPaths);
+					} catch (e) {
+						throw "Error parsing addtionalPaths. Make sure it is a valid JSON object (key/ value pairs).";
+					}
+				})();
+
+				for (const [local, remote] of parsedAdditionalPaths) {
+					await processPath(local, remote);
+				}
+			})
+			.then(() => {
+				console.log("Upload finished.");
+				return sftp.end();
+			})
+			.catch((err) => {
+				core.setFailed(`Action failed with error ${err}`);
+				process.exit(1);
+			});
+
+		async function processPath(local, remote) {
+			console.log("Uploading: " + local + " to " + remote);
+			if (fs.lstatSync(local).isDirectory()) {
+				return sftp.uploadDir(local, remote);
+			} else {
+				var directory = await sftp.realPath(path.dirname(remote));
+				if (!(await sftp.exists(directory))) {
+					await sftp.mkdir(directory, true);
+					console.log("Created directories.");
+				}
+
+				var modifiedPath = remote;
+				if (await sftp.exists(remote)) {
+					if ((await sftp.stat(remote)).isDirectory) {
+						var modifiedPath = modifiedPath + path.basename(local);
+					}
+				}
+
+				return sftp.put(fs.createReadStream(local), modifiedPath);
+			}
+		}
 
 		const sshClient = get_client(
 			row.get("host"),
